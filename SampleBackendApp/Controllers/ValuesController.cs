5e0fcab3b170ace739b0aa6570afa1507a52bb61
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-06-11 14:03:21 +0700

    Add project files.

---
+++ SampleBackendApp/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SampleBackendApp.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        private List<string> lstNama; 
+        public IEnumerable<string> Get()
+        {
+            lstNama = new List<string>
+            {
+                "erick", "bambang","joko","amir"
+            };
+            return lstNama;
+        }
+
+        // GET api/values/5
+        public string Get(string nama)
+        {
+            string result = lstNama.Where(n => n == nama).SingleOrDefault();
+            if (result != null)
+                return $"Ditemukan: {result}";
+            else
+                return "Data tidak ditemukan";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
