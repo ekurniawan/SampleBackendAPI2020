21f78333e479376f6fb7927d7ee9d19f8135a2dd
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-06-11 15:52:29 +0700

    menambahkan employeeDAL

---
+++ SampleBackendApp/DAL/EmployeeDAL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Configuration;
+using SampleBackendApp.Models;
+
+namespace SampleBackendApp.DAL
+{
+    public class EmployeeDAL
+    {
+        private string GetConnStr()
+        {
+            return WebConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            List<Employee> lstEmployee = new List<Employee>();
+            using(SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                string strSql = @"select * from Employee order by EmpName asc";
+                SqlCommand cmd = new SqlCommand(strSql,conn);
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        lstEmployee.Add(new Employee
+                        {
+                            EmpId = Convert.ToInt32(dr["EmpId"]),
+                            EmpName = dr["EmpName"].ToString(),
+                            Designation = dr["Designation"].ToString(),
+                            Department = dr["Department"].ToString(),
+                            Qualification = dr["Qualification"].ToString(),
+                            BirthDate = Convert.ToDateTime(dr["BirthDate"])
+                        });
+                    }
+                }
+                dr.Close();
+                cmd.Dispose();
+                conn.Close();
+            }
+            return lstEmployee;
+        }
+    }
+}
